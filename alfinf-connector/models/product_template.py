74b85eca9d5a730c240945e5dc64a22b190f2c95
pablomil2000 <martinlopezpablo@gmail.com>
2024-01-09 12:19:05 +0100

    Cambio formato en la cabecera

--- alfinf-connector/models/product_template.py
+++ alfinf-connector/models/product_template.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Manuel Calero - Alfinf
+# Copyright 2024 Pablo Martín - Alfinf
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models

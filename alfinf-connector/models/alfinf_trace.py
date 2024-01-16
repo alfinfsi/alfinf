51c04e80f14822c50b06e0e20208d4cfafb14d97
pablomil2000 <martinlopezpablo@gmail.com>
2024-01-16 12:46:52 +0100

    Add new fields to AlfinfTrace model and update view

--- alfinf-connector/models/alfinf_trace.py
+++ alfinf-connector/models/alfinf_trace.py
@@ -12,8 +12,24 @@ class AlfinfTrace(models.Model):
         string="Nombre",
         tracking=True
     )
+    tz_hectarea = fields.Char(
+        string="Traza hectarea"
+    )
+    tz_parcela = fields.Char(
+        string="Traza parcela"
+    )
+    tz_columna = fields.Char(
+        string="Traza columna"
+    )
+    tz_planta = fields.Char(
+        string="Traza planta"
+    )
     res_partner_id = fields.Many2one(
         string="Cliente",
         comodel_name="res.partner"
+    )
+    variedad_id = fields.Char(
+        string="Variedad"
+        #comodel_name="variedad"
     )
     active = fields.Boolean(default=True)
